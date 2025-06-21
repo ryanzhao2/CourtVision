035d4427f6c26c5935ec78a9fdc09be3cd4de1fa
Aditya Sen <a5sen@uwaterloo.ca>
2025-06-21 13:36:02 -0400

    setup api with mongodb

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -18,7 +18,7 @@ const LoginPage: React.FC = () => {
     setError("")
 
     try {
-      const response = await fetch('/login', {
+      const response = await fetch('http://127.0.0.1:5000/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
