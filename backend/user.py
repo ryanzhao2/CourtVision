7b5692ef4c3331f6d986965a3a721da5e608b44b
Aditya Sen <a5sen@uwaterloo.ca>
2025-06-21 13:46:10 -0400

    finish backend-frontend api login and sign-up

--- backend/user.py
+++ backend/user.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 from mongodb import create_user, get_user
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/signup", methods=["POST"])
 def signup():
